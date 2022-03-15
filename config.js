f221117aad3eca87e728b6390b9f394174f8fa9a
Jacob Anderson <63214293+JAndyson@users.noreply.github.com>
2022-03-15 18:24:25 -0500

    update

---
+++ config.js
@@ -0,0 +1,6 @@
+const blockfrostApiKey = {
+    0: "testnetWS8ic2FQlQxLBK0m58WVhe5e4dKgaQBp", // testnet
+    //1: "mainnetfqH0CVlBesnsj5IKhgIYCn231KzqUOyk" // mainnet
+}
+
+export default blockfrostApiKey;
